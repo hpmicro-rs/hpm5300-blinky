7f5dedcbf631768273063a3f21255578a7cceb4d
Andelf <andelf@gmail.com>
2024-06-14 23:45:52 +0800

    fix: run-openocd

--- run-openocd.sh
+++ run-openocd.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 
-OPENOCD_CFG=/Users/mono/Elec/HPM/hpm_sdk/boards/openocd
 
-openocd -f $OPENOCD_CFG/probes/ft2232.cfg -f $OPENOCD_CFG/soc/hpm5300.cfg -f $OPENOCD_CFG/boards/hpm5300evk.cfg
+export HPM_SDK_BASE=/path/to/hpm_sdk
+
+# openocd -f $OPENOCD_CFG/probes/ft2232.cfg -f $OPENOCD_CFG/soc/hpm5300.cfg -f $OPENOCD_CFG/boards/hpm5300evk.cfg
+
+export BOARD=hpm5301evklite
+# export BOARD=hpm5300evk
+export PROBE=cmsis_dap
+# export PROBE=ft2232
+
+openocd -c "set HPM_SDK_BASE ${HPM_SDK_BASE}; set BOARD ${BOARD}; set PROBE ${PROBE};" -f ${HPM_SDK_BASE}/boards/openocd/hpm5300_all_in_one.cfg
